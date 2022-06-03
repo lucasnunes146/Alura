2383b65d80fae755cdd789e4c45d951ef8ad1c3c
lucasnunes146 <lucasnunes146@gmail.com>
2022-06-03 09:22:39 -0300

    Commits de aprendizado

---
+++ front-end/contador/assets/js/scripts.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+var CURRENT_NUMBER = document.getElementById('currentNumber');
+
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+    count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+
+
+function test() {
+	kdowkdpo;
+}
---
+++ front-end/js/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,22 @@
+console.log("Trabalhando com atribuição de variáveis");
+let idade = 29;
+let primeiroNome = "Lucas";  //com o let atribuido, é possível alterar uma variável que era constante
+const sobrenome = "Nunes";
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é: ${primeiroNome} ${sobrenome}`);
+
+nome = primeiroNome + " " + sobrenome;
+
+console.log(nome);
+
+let contador = 0;
+contador += 1;
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+// nomeCompleto = 2;
+
+idade = 26;
+idade = 26+1;
+console.log(idade);
